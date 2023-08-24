cac6e3e0af4ef59cc7ddb019a71394888144c26f
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-24 07:56:49 +0200

    no error txt for pall

--- pall.c
+++ pall.c
@@ -9,12 +9,6 @@ void _print(stack_t **h, unsigned int line_number)
 {
 	stack_t *curr;
 
-	if  (*h == NULL)
-	{
-		fprintf(stderr, "L%i: can't pall, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
 	curr = *h;
 	while (curr != NULL)
 	{
